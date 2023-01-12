6f02491e513ba3d9a66746f67cfc8019d43a7de5
Oleksandr Khomenko <sasha.khomenko12@gmail.com>
2023-01-12 17:45:53 +0200

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = null;
+        try (FileInputStream fileInputStream = new FileInputStream(file)){
+            String readFileContent = new String(fileInputStream.readAllBytes());
+            String[] copyFileContent = readFileContent.split("\\R\n");
+            String name = copyFileContent[0].split(" ")[1];
+            Integer age = Integer.parseInt(copyFileContent[1].split(" ")[1]);
+            String email = copyFileContent[2].split(" ")[1];
+            Long phone = Long.parseLong(copyFileContent[3].split(" ")[1]);
+            profile = new Profile(name, age, email, phone);
+        } catch (IOException iOE){
+            System.out.println("Catched)");
+        }
+        return profile;
     }
 }
